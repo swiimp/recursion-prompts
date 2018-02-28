98f1011cc857385a52947f4494ffe45f4eefd049
Robin Markowitz <loepoe71@gmail.com>
2018-02-27 19:28:52 -0800

    Implement arraySum

--- src/recursion.js
+++ src/recursion.js
@@ -32,6 +32,15 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  if (Array.isArray(array[0])) {
+    return arraySum(array[0]) + arraySum(array.slice(1));
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  else {
+    return array[0] + arraySum(array.slice(1));
+  }
 };
 
 // 4. Check if a number is even.
