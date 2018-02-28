f8cd5555c385454d5884f720f712fc2107924d0a
Robin Markowitz <loepoe71@gmail.com>
2018-02-27 21:18:27 -0800

    Add full exponent implementation
    This includes the base tests and the two additional tests that are disabled by default.

--- src/recursion.js
+++ src/recursion.js
@@ -96,6 +96,24 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+  else if (exp === 1) {
+    return base;
+  }
+  else if (exp > 0) {
+    if (exp % 2 === 0) {
+      var result = exponent(base, (exp/2));
+      return result * result;
+    }
+    else {
+      return exponent(base, exp - 1) * base;
+    }
+  }
+  else {
+    return (1 / exponent(base, -exp));
+  }
 };
 
 // 8. Determine if a number is a power of two.
